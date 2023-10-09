3037fd15a38942aebdffc96abd40e185a7236aa8
Mélanie Geulin <117943770+MelanieGeulin@users.noreply.github.com>
2023-10-09 14:56:04 +0200

    Update Exo10_transpose_matrix.py

--- Exo10_transpose_matrix.py
+++ Exo10_transpose_matrix.py
@@ -1,6 +1,6 @@
 """
 PROGRAM NAME - transpose_matrix
-PROGRAMMER - Kuriozity
+PROGRAMMER - Mélanie Geulin (melanie.geulin@gmail.com)
 LANGUAGE - Python + numpy
 SYSTEM - Windows 11
 DATE - Completed 21/06/2023
